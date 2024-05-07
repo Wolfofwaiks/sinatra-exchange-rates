e7859e621842c3da1d460e0a43606b5a234a4092
Wolfofwaiks <165302970+Wolfofwaiks@users.noreply.github.com>
2024-05-07 02:01:19 +0000

    step two

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require "sinatra/reloader"
 require "http"
 
 get("/") do
-  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=EXCHANGE_RATE")
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE")}")
 
   @string_response = @raw_response.to_s
 
@@ -18,6 +18,26 @@ get ("/:first_symbol") do
 
   @the_symbol = params.fetch("first_symbol")
 
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE")}")
+
+  @string_response = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@string_response)
+
+  @currencies = @parsed_response.fetch("currencies")
+
   erb(:step_one)
+
+end 
+
+
+get ("/:first_symbol/:second_symbol") do
+
+  @from = params.fetch("first_symbol")
+  @to = params.fetch("second_symbol")
+
+  erb(:step_two)
+
+ 
 
 end 
